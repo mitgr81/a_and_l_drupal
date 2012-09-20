160d98bbac295c01c22fdafbc2e27c9d8cd2abcc
Chris McGraw <chris.mcgraw@lodgenet.com>
2012-09-20 00:50:59 -0500

    Rewrite to Zen, implementation of previewer

---
+++ drupal/sites/all/modules/custom/ald_previewer/ald_previewer.tpl.php
@@ -0,0 +1,4 @@
+<div id='ald-echo'></div>
+<textarea id="ald-settings"><?php print $ald_settings;?></textarea>
+
+<input class="ald-previewer"></input>
---
+++ drupal/sites/all/modules/custom/ald_previewer/js/previewer.js
@@ -0,0 +1,35 @@
+(function ($) {
+    $(document).ready(function(){
+        var settings = JSON.parse($('#ald-settings').val()),
+        theme = settings.theme,
+        name = settings.name,
+        path = settings.path + theme + '/';
+
+
+        $('input.ald-previewer').bind('keyup', function(event){
+            var text = $(event.target).val(),
+                $echo = $('#ald-echo');
+
+            $echo.html(text);
+            if(text === ''){
+                $echo.fadeOut();
+            }else{
+                $echo.empty();
+                for (var textchar in text) {
+                    var my_character = text.charAt(textchar),
+                        $img = $('<img>');
+                    $img.attr('src', path + my_character.toLowerCase() + '.png');
+                    $img.attr('alt', my_character);
+                    $img.attr('title', my_character);
+                    $echo.append($img);
+                }
+                $echo.fadeIn();
+            }
+        });
+        if(name !== '') {
+            $('input.ald-previewer').val(name).trigger('keyup');
+
+        }
+    });
+
+}(jQuery));
