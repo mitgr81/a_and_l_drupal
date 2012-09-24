3e4ec2db82bb17271380f6db5fa0f48c1f636577
Chris McGraw <chris.mcgraw@lodgenet.com>
2012-09-23 20:54:12 -0500

    First pass at adding ald content type and welcome
    This should be a good place to start with the letters, I think.
    
    [Content Type]

---
+++ drupal/sites/all/modules/custom/ald_alphabetter/ald_alphabetter_welcome.tpl.php
@@ -0,0 +1,19 @@
+Welcome to the Babybets webpage!<br/>
+My name is Anne. I have worked with children and families for 28 years as a registered nurse in pediatrics, childcare, and as a parent.<br/>
+
+My sister, Lisa, and I designed Babybet photos after realizing that there are all kinds of letter photos for adults but none for babies. We looked at MANY toys and baby items to find the most adorable ones for your baby’s letter photos. Lisa is an expert photographer and Adobe Photoshop artist.<br/>
+
+Lisa’s son, Chris, is a software engineer. He helped us with the technical portion of our business.<br/>
+
+My husband, Richard, is our accountant.<br/>
+
+We are truly a family business with families in mind. We have many ideas for new letters and alphabets so check back with us frequently!<br/>
+
+Our blog contains ideas for grandparent gifts and room decorating.<br/>
+
+Please send us photos or ideas in our blog of how you incorporated your Babybets photos into your child’s room or play space. Babybets make unique gifts for newborns to three year olds!<br/>
+<br/>
+ ~ Anne
+
+
+
