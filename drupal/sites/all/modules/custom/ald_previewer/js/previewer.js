8db206f5e974cc859d3dc657b8c99219a6ac1849
Chris McGraw <chris.mcgraw@lodgenet.com>
2012-12-08 01:49:39 -0600

    Adds a previewer block, gets a bit moving on the adorabets CSS
    need to pull stuff out of normalize.css

--- drupal/sites/all/modules/custom/ald_previewer/js/previewer.js
+++ drupal/sites/all/modules/custom/ald_previewer/js/previewer.js
@@ -16,12 +16,14 @@
             }else{
                 $echo.empty();
                 for (var textchar in text) {
-                    var my_character = text.charAt(textchar),
-                        $img = $('<img>').addClass('ald-image');
-                    $img.attr('src', path + my_character.toLowerCase() + '.png');
-                    $img.attr('alt', my_character);
-                    $img.attr('title', my_character);
-                    $echo.append($img);
+                    if (text.charAt(textchar).match(/[a-zA-Z]/)){
+                        var my_character = text.charAt(textchar),
+                            $img = $('<img>').addClass('ald-image');
+                        $img.attr('src', path + my_character.toLowerCase() + '.png');
+                        $img.attr('alt', my_character);
+                        $img.attr('title', my_character);
+                        $echo.append($img);
+                    }
                 }
                 $echo.fadeIn();
             }
